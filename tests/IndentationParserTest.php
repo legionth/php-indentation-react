e3535396f9755516932fe8b9b1db136e4116e0e4
Niels Theen <werwofl@googlemail.com>
2016-09-29 12:17:44 +0200

    Added Parser to indent lines in a stream

---
+++ tests/IndentationParserTest.php
@@ -0,0 +1,95 @@
+<?php
+
+use React\Stream\ReadableStream;
+use Legionth\React\Indentation\IndentationParser;
+
+class IndentationParserTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->input = new ReadableStream();
+        $this->parser = new IndentationParser($this->input);
+    }
+    
+    public function testMethodIsCalled()
+    {
+        $this->parser->on('data', $this->expectCallableOnceWith('    hello'));
+        $this->input->emit('data', array('hello'));
+    }
+    
+    public function testMethodIsCalledTwoTimes()
+    {
+        $this->parser->on('data', $this->expectCallableConsecutive(2, array('    hello', 'world')));
+        
+        $this->input->emit('data', array('hello'));
+        $this->input->emit('data', array('world'));
+    }
+    
+    public function testWithLineBreak()
+    {
+        $this->parser->on('data', $this->expectCallableOnceWith('    hello' . PHP_EOL .'    world'));
+        
+        $this->input->emit('data', array('hello' . PHP_EOL . 'world'));
+    }
+    
+    public function testHandleClose()
+    {
+        $this->parser->on('close', $this->expectCallableOnce());
+    
+        $this->input->close();
+    
+        $this->assertFalse($this->parser->isReadable());
+    }
+    
+    public function testHandleError()
+    {
+        $this->parser->on('error', $this->expectCallableOnce());
+        $this->parser->on('close', $this->expectCallableOnce());
+    
+        $this->input->emit('error', array(new \RuntimeException()));
+    
+        $this->assertFalse($this->parser->isReadable());
+    }
+    
+    public function testPauseStream()
+    {
+        $input = $this->getMock('React\Stream\ReadableStreamInterface');
+        $input->expects($this->once())->method('pause');
+        
+        $parser = new IndentationParser($input);
+        $parser->pause();
+    }
+    
+    public function testResumeStream()
+    {
+        $input = $this->getMock('React\Stream\ReadableStreamInterface');
+        $input->expects($this->once())->method('pause');
+        
+        $parser = new IndentationParser($input);
+        $parser->pause();
+        $parser->resume();
+    }
+    
+    public function testPipeStream()
+    {
+        $dest = $this->getMock('React\Stream\WritableStreamInterface');
+        
+        $ret = $this->parser->pipe($dest);
+        
+        $this->assertSame($dest, $ret);
+    }
+    
+    private function expectCallableConsecutive($numberOfCalls, array $with)
+    {
+        $mock = $this->createCallableMock();
+    
+        for ($i = 0; $i < $numberOfCalls; $i++) {
+            $mock
+                ->expects($this->at($i))
+                ->method('__invoke')
+                ->with($this->equalTo($with[$i]));
+        }
+    
+        return $mock;
+    }
+}
